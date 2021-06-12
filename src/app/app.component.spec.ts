3d71e716ac6dbf38209abe2bddf3246d436fd4cc
KV <aswin.kv@siemens.com>
2021-06-12 22:01:59 +0530

    Addes specs

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,96 @@
-import { TestBed, async } from '@angular/core/testing';
+import { Component, EventEmitter, Input, NO_ERRORS_SCHEMA, Output, ViewChild } from '@angular/core';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { MatSort } from '@angular/material/sort';
 import { AppComponent } from './app.component';
+import { ELEMENT_DATA } from './data.const';
+import { Roles } from './roles.enum';
+import { TableContent } from './table-content';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+
+  @Component({
+    selector: 'app-table-component',
+    template: '<div>Mock Template</div>',
+  })
+  class FakeTableComponent {
+
+    @ViewChild(MatSort) sort: MatSort;
+    @Input() roles: Roles;
+    @Input() dataSourceInput: Array<TableContent>
+    @Output() emitNumberOfItemsSelected: EventEmitter<number> = new EventEmitter<number>();
+  
+  }
+
   beforeEach(async(() => {
+
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+        AppComponent, FakeTableComponent
+      ]
+    });
+
+    fixture = TestBed.createComponent(AppComponent);
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+  it('should add the value (10) to selectedItem', () => {
+    const testingValue = 10;
+    fixture.componentInstance.onSelectionOfItem(testingValue);
 
-  it(`should have as title 'table'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('table');
-  });
+    expect(fixture.componentInstance.selectedItem).toBe(testingValue);
+  })
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('should add the value (0) to selectedItem', () => {
+    const testingValue = 0;
+    fixture.componentInstance.onSelectionOfItem(testingValue);
+
+    expect(fixture.componentInstance.selectedItem).toBe(testingValue);
+  })
+  
+  it('should add the value (-10) to selectedItem', () => {
+    const testingValue = -10;
+    fixture.componentInstance.onSelectionOfItem(testingValue);
+
+    expect(fixture.componentInstance.selectedItem).toBe(testingValue);
+  })
+    
+  it('should the count in template and selected item must be same', () => {
+    const testingValue = 10;
+    fixture.componentInstance.onSelectionOfItem(testingValue);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('table app is running!');
+    
+    const countInTemplate = fixture.nativeElement.querySelector('h2>.highlight').textContent;
+    expect(Number(countInTemplate)).toBe(testingValue);
+  })
+      
+  it('should the hide h2 in template if the value is less than 1', () => {
+    const testingValue = 0;
+    fixture.componentInstance.onSelectionOfItem(testingValue);
+    fixture.detectChanges();
+
+    const countInTemplate = fixture.nativeElement.querySelector('h2');
+    expect(countInTemplate).toBeNull();
+  })
+  
+  it('shold check the element data is getting updated in the template', () => {
+    
+    fixture.componentInstance.dataSource = ELEMENT_DATA;
+    fixture.componentInstance.selectedItem = 1
+    fixture.detectChanges();
+
+    const countInTemplate = fixture.nativeElement.querySelector('#totalLength').textContent;
+    
+    expect(Number(countInTemplate)).toBe(ELEMENT_DATA.length);
   });
+
+  it('should print the values in proper sentence', () => {
+    const selectedValue = 4;
+    fixture.componentInstance.dataSource = ELEMENT_DATA;
+    fixture.componentInstance.selectedItem = selectedValue;
+    fixture.detectChanges();
+
+    const textinTemplate = fixture.nativeElement.querySelector('h2').textContent;
+    
+    expect(textinTemplate).toBe(`Users ${selectedValue} selected out of ${ELEMENT_DATA.length}`);
+  })
 });
--- src/app/table-component/table-component.component.spec.ts
+++ src/app/table-component/table-component.component.spec.ts
@@ -1,25 +1,70 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatTableDataSource } from '@angular/material/table';
+import { ELEMENT_DATA } from '../data.const';
+import { TableContent } from '../table-content';
 
 import { TableComponentComponent } from './table-component.component';
 
 describe('TableComponentComponent', () => {
-  let component: TableComponentComponent;
   let fixture: ComponentFixture<TableComponentComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TableComponentComponent ]
-    })
-    .compileComponents();
+      declarations: [TableComponentComponent],
+
+      schemas: [NO_ERRORS_SCHEMA]
+    });
+
+    fixture = TestBed.createComponent(TableComponentComponent);
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TableComponentComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(fixture.componentInstance).toBeTruthy();
   });
+
+  it('should return true if all selected', () => {
+    const selectedTestValue = 6;
+    fixture.componentInstance.selection.selected.length = selectedTestValue;
+    fixture.componentInstance.dataSource = new MatTableDataSource<TableContent>(ELEMENT_DATA);
+
+    const resultValue = fixture.componentInstance.isAllSelected();
+
+    expect(resultValue).toBeTrue();
+
+  });
+
+
+  it('should return false if not all selected', () => {
+    const selectedTestValue = 0;
+    fixture.componentInstance.selection.selected.length = selectedTestValue;
+    fixture.componentInstance.dataSource = new MatTableDataSource<TableContent>(ELEMENT_DATA);
+
+    const resultValue = fixture.componentInstance.isAllSelected();
+
+    expect(resultValue).toBeFalse();
+
+  })
+
+  it('should give proper value for displayColoum', () => {
+
+    const result = fixture.componentInstance.displayColoum(ELEMENT_DATA);
+    const expectedResult = ["select", "id", "name", "email", "roles", "action"];
+    
+    expect(result).toEqual(expectedResult);
+
+  })
+
+  it('should give proper value for checkboxLabel', () => {
+
+    const result = fixture.componentInstance.checkboxLabel(ELEMENT_DATA[0]);
+    //fixture.detectChanges();
+    const labelForAllSelection = 'select row 2';
+
+    
+    expect(result).toEqual(labelForAllSelection);
+
+  })
+
 });
